2bd7221a524def426c8c4cbc7cca39157f4be95b
lliu <lliu@roku.com>
2019-01-02 23:34:27 -0800

    Initial commit

---
+++ src/main/scala/chapter03/List.scala
@@ -0,0 +1,107 @@
+package chapter03
+
+sealed trait List[+A]
+
+case object Nil extends List[Nothing]
+
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+object List {
+
+  def apply[A](as: A*): List[A] =
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+
+  // Exercise 3.2
+  def tail[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(_, t) => t
+  }
+
+  // Exercise 3.3
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+    case Nil => Cons(h, Nil)
+    case Cons(_, t) => Cons(h, t)
+  }
+
+  // Exercise 3.4
+  def drop[A](l: List[A], n: Int): List[A] = l match {
+    case Nil => Nil
+    case Cons(_, t) if n > 0 => drop(t, n - 1)
+    case _ => l
+  }
+
+  // Exercise 3.5
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+    case Nil => Nil
+    case Cons(h, t) if f(h) => dropWhile(t, f)
+    case _ => l
+  }
+
+  // Exercise 3.6
+  def init[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(_, Nil) => Nil
+    case Cons(h, t) => Cons(h, init(t))
+  }
+
+  // Listing 3.2
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = as match {
+    case Nil => z
+    case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+  }
+
+  // Exercise 3.7
+  def sum2(ns: List[Int]): Int =
+    foldRight(ns, 0)(_ + _)
+
+  def product2(ns: List[Double]): Double =
+    foldRight(ns, 1.0)(_ * _)
+
+  // Exercise 3.9
+  def length[A](l: List[A]): Int =
+    foldRight(l, 0)((_, y) => 1 + y)
+
+  // Exercise 3.10
+  def foldLeft[A, B](l: List[A], z: B)(f: (B, A) => B): B = l match {
+    case Nil => z
+    case Cons(h, t) => foldLeft(t, f(z, h))(f)
+  }
+
+  // Exercise 3.11
+  def sum3(ns: List[Int]): Int =
+    foldLeft(ns, 0)(_ + _)
+
+  def product3(ns: List[Double]): Double =
+    foldLeft(ns, 1.0)(_ * _)
+
+  def length3[A](l: List[A]): Int =
+    foldLeft(l, 0)((x, _) => x + 1)
+
+  // Exercise 3.12
+  def reverse[A](l: List[A]): List[A] = {
+    foldLeft(l, Nil: List[A])((xs, x) => Cons(x, xs))
+  }
+
+  // Exercise 3.13
+  def foldLeftUseFoldRight[A, B](l: List[A], z: B)(f: (B, A) => B): B =
+    foldRight(reverse(l), z)((x, xs) => f(xs, x))
+
+  def foldRightUseFoldLeft[A, B](l: List[A], z: B)(f: (A, B) => B): B =
+    foldLeft(reverse(l), z)((xs, x) => f(x, xs))
+
+  def append[A](a1: List[A], a2: List[A]): List[A] = a1 match {
+    case Nil => a2
+    case Cons(h, t) => Cons(h, append(t, a2))
+  }
+
+  // Exercise 3.14
+  def appendUseFoldRight[A](a1: List[A], a2: List[A]): List[A] = {
+    foldRight(a1, a2)((x, xs) => Cons(x, xs))
+  }
+
+  //Exercise 3.15
+  def concat[A](lists: List[List[A]]): List[A] = {
+    foldRight(lists, Nil: List[A])((x, y) => appendUseFoldRight(x, y))
+  }
+}
---
+++ src/test/scala/chapter03/Chapter03Test.scala
@@ -0,0 +1,97 @@
+package chapter03
+
+import chapter03.List._
+import org.scalatest.FunSuite
+
+class Chapter03Test extends FunSuite {
+
+  val l = List(1, 2, 3, 4, 5)
+  val s = List(6, 7, 8, 9)
+  val dl = List(1.0, 2.0, 3.0, 4.0, 5.0)
+
+  test("Exercise 3.1") {
+    // Exercise 3.1
+    val x = List(1, 2, 3, 4, 5) match {
+      case Cons(a, Cons(2, Cons(4, _))) => a
+      case Nil => 42
+      case Cons(a, Cons(b, Cons(3, Cons(4, _)))) => a + b
+      case Cons(h, _) => h
+      case _ => 101
+    }
+    assert(x == 3)
+  }
+
+  // Exercise 3.2
+  test("Exercise 3.2") {
+    assert(tail(Nil) == Nil)
+    assert(tail(s) == List(7, 8, 9))
+    assert(tail(tail(tail(l))) == List(4, 5))
+  }
+
+  // Exercise 3.3
+  test("Exercise 3.3") {
+    assert(setHead(Nil, 5) == Cons(5, Nil))
+    assert(setHead(l, 7) == List(7, 2, 3, 4, 5))
+  }
+
+  // Exercise 3.4
+  test("Exercise 3.4") {
+    assert(drop(l, 3) == List(4, 5))
+    assert(drop(l, 5) == Nil)
+    assert(drop(l, 6) == Nil)
+  }
+
+  // Exercise 3.5
+  test("Exercise 3.5") {
+    assert(dropWhile(l, (x: Int) => x < 4) == List(4, 5))
+    assert(dropWhile(l, (x: Int) => x < 100) == Nil)
+  }
+
+  // Exercise 3.6
+  test("Exercise 3.6") {
+    assert(init(l) == List(1, 2, 3, 4))
+    assert(init(Nil) == Nil)
+    assert(init(List(1)) == Nil)
+  }
+
+  // Exercise 3.7
+  test("Exercise 3.7") {
+    assert(sum2(l) == 15)
+    assert(product2(dl) == 120.0)
+  }
+
+  // Exercise 3.8
+  test("Exercise 3.8") {
+    assert(foldRight(l, Nil: List[Int])(Cons(_, _)) == l)
+  }
+
+  // Exercise 3.9
+  test("Exercise 3.9") {
+    assert(length(l) == 5)
+    assert(length(s) == 4)
+  }
+
+  // Exercise 3.11
+  test("Exercise 3.11") {
+    assert(sum3(l) == 15)
+    assert(product3(dl) == 120.0)
+    assert(length3(l) == 5)
+  }
+
+  // Exercise 3.12
+  test("Exercise 3.12") {
+    assert(reverse(Nil) == Nil)
+    assert(reverse(l) == List(5, 4, 3, 2, 1))
+  }
+
+  // Exercise 3.14
+  test("Exercise 3.14") {
+    assert(appendUseFoldRight(l, s) == List(1, 2, 3, 4, 5, 6, 7, 8, 9))
+  }
+
+  // Exercise 3.15
+  test("Exercise 3.15") {
+    assert(concat(List(l, s)) == List(1, 2, 3, 4, 5, 6, 7, 8, 9))
+  }
+
+}
